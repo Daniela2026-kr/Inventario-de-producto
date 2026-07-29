c28a67e68ddd6f4703eaa46f51e7cd03a5a5bcee
puentesroxana57-sys <puentesroxana57@gmail.com>
2026-07-29 15:43:20 -0600

    Add files via upload
    Agregue la opción de carrito

---
+++ inventario.py
@@ -0,0 +1,25 @@
+# Inventario de maquillaje
+
+inventario = {
+    "Labial": {"precio": 6.00, "disponibles": 20},
+    "Base": {"precio": 12.50, "disponibles": 15},
+    "Corrector": {"precio": 8.00, "disponibles": 10},
+    "Rubor": {"precio": 7.50, "disponibles": 12},
+    "Paleta": {"precio": 18.00, "disponibles": 8},
+    "Delineador": {"precio": 4.50, "disponibles": 8},
+    "Máscara de pestañas": {"precio": 10.00, "disponibles": 20},
+    "Iluminador": {"precio": 7.25, "disponibles": 10},
+    "Lápiz de cejas": {"precio": 3.99, "disponibles": 30},
+    "Set de esponjas y brochas": {"precio": 15.00, "disponibles": 15}
+}
+
+carrito = []
+
+while True:
+    print("\n===== TIENDA DE MAQUILLAJE =====")
+    print("1. Ver productos")
+    print("2. Comprar producto")
+    print("3. Ver carrito")
+    print("4. Salir")
+
+     
